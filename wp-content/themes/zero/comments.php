d04aa4f9c1b72490fd95ab1762933334e7bcd613
Mohammed Mostafa <m5z.mohammed@gmail.com>
2019-10-01 08:23:21 +0200

    comments2

--- wp-content/themes/zero/comments.php
+++ wp-content/themes/zero/comments.php
@@ -1,6 +1,10 @@
 <?php
 
 // If comments are open or we have at least one comment, load up the comment template.
-if ( comments_open() || get_comments_number() ) :
-    wp_list_comments();
-endif;
+if ( comments_open() || get_comments_number() ) {
+    $comments_args = array(
+        'max_depth'  => 5,
+        'type'       => 'comment'
+    );
+    wp_list_comments($comments_args);
+}
